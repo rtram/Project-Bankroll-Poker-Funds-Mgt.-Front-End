bd8ff70d902627d09df3c6bd08a6763f89e02a68
Robin Tram <robin.tram@gmail.com>
2019-01-09 11:35:22 -0500

    First Commit.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Login from './Components/Login'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Login />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
