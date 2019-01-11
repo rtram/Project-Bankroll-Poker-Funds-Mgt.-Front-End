1a24b178010f3542fe407beb91244bc89b18bc52
Robin Tram <robin.tram@gmail.com>
2019-01-10 22:49:51 -0500

    Added Semantic UI Base Layout.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,116 @@
 import React, { Component } from 'react';
 import './App.css';
+
+import NavBar from './react/navbar.js'
 import Login from './react/login/login.js'
 import Dashboard from './react/homepage/dashboard.js'
+
+import {
+  Container,
+  Divider,
+  Dropdown,
+  Grid,
+  Header,
+  Image,
+  List,
+  Menu,
+  Segment,
+} from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+      <Menu fixed='top' inverted>
+        <Container>
+          <Menu.Item as='a' header>
+            <Image size='mini' src='/logo.png' style={{ marginRight: '1.5em' }} />
+            Bankroll Project
+          </Menu.Item>
+          <Menu.Item as='a'>Home</Menu.Item>
+
+          <Dropdown item simple text='Dropdown'>
+            <Dropdown.Menu>
+              <Dropdown.Item>List Item</Dropdown.Item>
+              <Dropdown.Item>List Item</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Header>Header Item</Dropdown.Header>
+              <Dropdown.Item>
+                <i className='dropdown icon' />
+                <span className='text'>Submenu</span>
+                <Dropdown.Menu>
+                  <Dropdown.Item>List Item</Dropdown.Item>
+                  <Dropdown.Item>List Item</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown.Item>
+              <Dropdown.Item>List Item</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Container>
+      </Menu>
+
+        <Container text style={{ marginTop: '7em' }}>
+          <NavBar />
           <Login />
           <Dashboard />
-        </header>
+        </Container>
+
+        <Segment inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em' }}>
+          <Container textAlign='center'>
+            <Grid divided inverted stackable>
+              <Grid.Column width={3}>
+                <Header inverted as='h4' content='Group 1' />
+                <List link inverted>
+                  <List.Item as='a'>Link One</List.Item>
+                  <List.Item as='a'>Link Two</List.Item>
+                  <List.Item as='a'>Link Three</List.Item>
+                  <List.Item as='a'>Link Four</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <Header inverted as='h4' content='Group 2' />
+                <List link inverted>
+                  <List.Item as='a'>Link One</List.Item>
+                  <List.Item as='a'>Link Two</List.Item>
+                  <List.Item as='a'>Link Three</List.Item>
+                  <List.Item as='a'>Link Four</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <Header inverted as='h4' content='Group 3' />
+                <List link inverted>
+                  <List.Item as='a'>Link One</List.Item>
+                  <List.Item as='a'>Link Two</List.Item>
+                  <List.Item as='a'>Link Three</List.Item>
+                  <List.Item as='a'>Link Four</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={7}>
+                <Header inverted as='h4' content='Footer Header' />
+                <p>
+                  Extra space for a call to action inside the footer that could help re-engage users.
+                </p>
+              </Grid.Column>
+            </Grid>
+
+            <Divider inverted section />
+            <Image centered size='mini' src='/logo.png' />
+            <List horizontal inverted divided link size='small'>
+              <List.Item as='a' href='#'>
+                Site Map
+              </List.Item>
+              <List.Item as='a' href='#'>
+                Contact Us
+              </List.Item>
+              <List.Item as='a' href='#'>
+                Terms and Conditions
+              </List.Item>
+              <List.Item as='a' href='#'>
+                Privacy Policy
+              </List.Item>
+            </List>
+          </Container>
+        </Segment>
       </div>
     );
   }
---
+++ src/react/navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class NavBar extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        NavBar
+      </div>
+    )
+  }
+}
